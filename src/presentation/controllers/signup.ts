dcf36ab7ffae4e76f30ed65dc0408a765e5317b4
Adriano hilario <adrianoferreira31@gmail.com>
2022-08-25 14:51:14 -0300

    feat: ensure SignUpController returns 500 if EmailValidator thows

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,3 +1,4 @@
+import { ServerError } from './../errors/server-error';
 import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
@@ -7,19 +8,27 @@ import { InvalidParamError } from '../errors/invalid-param-error';
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator;
 
-    constructor ( emailValidator: EmailValidator) {
+    constructor(emailValidator: EmailValidator) {
         this.emailValidator = emailValidator;
     }
-    handle (httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = [ 'name', 'email', 'password', 'passwordConfirmation' ];
-        for (const field of requiredFields) {
-            if(!httpRequest.body[field]){
-                return badRequest(new MissingParamError(field))
+    handle(httpRequest: HttpRequest): HttpResponse {
+        try {
+            const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
+            }
+            const isValid = this.emailValidator.isValid(httpRequest.body.email)
+            if (!isValid) {
+                return badRequest(new InvalidParamError('email'))
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: new ServerError()
             }
         }
-        const isValid = this.emailValidator.isValid(httpRequest.body.email)
-        if (!isValid) {
-            return badRequest(new InvalidParamError('email'))
-        }
+
     }
 }
